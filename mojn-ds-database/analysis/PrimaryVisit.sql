1f34cd0cb8c793ad8d77abbfe003b52f1764e2a2
Sarah Wright <s.wright.618@gmail.com>
2018-07-26 16:11:48 -0700

    Added analysis query that lists spring visits where visit type is primary

---
+++ mojn-ds-database/analysis/PrimaryVisit.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [analysis].[PrimaryVisit]
+AS SELECT
+		park.Code AS Park,
+		spring.Code AS SpringCode,
+		spring.Name AS SpringName,
+		visit.VisitDate,
+		visit.StartTime,
+		s_type.Label AS SpringType
+	FROM [data].[Site] As spring 
+	INNER JOIN [data].[Visit] AS visit ON visit.SiteID = spring.ID
+	INNER JOIN [lookup].[Park] AS park ON spring.ParkID = park.ID
+	INNER JOIN [lookup].[SpringType] AS s_type ON visit.SpringTypeID = s_type.ID
+	WHERE [visit].[VisitTypeID] = 1
+
