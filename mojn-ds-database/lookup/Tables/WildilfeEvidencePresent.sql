1118a1fbc4063ce7e2f2a63d6ce3aa61a86171de
Sarah Wright <s.wright.618@gmail.com>
2018-11-05 17:09:49 -0800

    Retired old wildlife table and created new wildlife table that better reflects the data collection SOPs

---
+++ mojn-ds-database/lookup/Tables/WildilfeEvidencePresent.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [lookup].[WildlifeEvidencePresent] (
+    [ID]    TINYINT      IDENTITY (1, 1) NOT NULL,
+    [Code]  VARCHAR (6)  NOT NULL,
+    [Label] VARCHAR (20) NOT NULL,
+    CONSTRAINT [PK_WildlifeEvidencePresent] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Indicates whether a specific wildilfe evidence type was found at the spring', @level0type = N'SCHEMA', @level0name = N'lookup', @level1type = N'TABLE', @level1name = N'WildlifeEvidencePresent';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Primary key for this table', @level0type = N'SCHEMA', @level0name = N'lookup', @level1type = N'TABLE', @level1name = N'WildlifeEvidencePresent', @level2type = N'COLUMN', @level2name = N'ID';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Unique code for wildlife evidence presence', @level0type = N'SCHEMA', @level0name = N'lookup', @level1type = N'TABLE', @level1name = N'WildlifeEvidencePresent', @level2type = N'COLUMN', @level2name = N'Code';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Unique name for wildlife evidence presence', @level0type = N'SCHEMA', @level0name = N'lookup', @level1type = N'TABLE', @level1name = N'WildlifeEvidencePresent', @level2type = N'COLUMN', @level2name = N'Label';
+
