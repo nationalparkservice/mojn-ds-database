f466b55d0e5aa18e35e97640a30a6fd100ab82e4
Sarah Wright <s.wright.618@gmail.com>
2018-11-26 11:26:43 -0800

    Updated analysis views to reflect changes to estimated discharge SOP

---
+++ mojn-ds-database/analysis/Views/RETIRED_DischargeEstimated.sql
@@ -0,0 +1,21 @@
+﻿CREATE VIEW analysis.RETIRED_DischargeEstimated
+AS
+SELECT      d.ParkCode, 
+			d.SiteCode,
+			d.SiteName,
+			d.VisitDate,
+			d.VisitGroup, 
+            d.FlowCondition,
+			df.Code AS DischargeFlag,
+			de.Discharge_L_per_min,
+			d.VisitType, 
+            d.DPL
+FROM        intermediate.Discharge AS d
+			INNER JOIN data.RETIRED_DischargeEstimatedObservation AS de
+			ON d.DischargeActivityID = de.DischargeActivityID
+			LEFT OUTER JOIN lookup.DischargeEstimatedFlag AS df
+			ON de.DischargeEstimatedFlagID = df.ID
+GO
+
+
+
