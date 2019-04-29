88fc6bb2ba56d588e3765ad142db816588840eb7
Sarah Wright <s.wright.618@gmail.com>
2019-04-29 12:06:50 -0700

    Add analysis views for sensors

---
+++ mojn-ds-database/analysis/Views/SensorsCurrentlyDeployed.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [analysis].[SensorsCurrentlyDeployed]
+AS
+SELECT      s.Label AS SensorNumber,
+			s.SerialNumber AS SerialNumber,
+			ds.Code AS SpringCode,
+			ds.Name AS SpringName,
+			dv.VisitDate AS DeploymentDate
+FROM        ref.Sensor AS s
+			LEFT JOIN data.SensorDeployment AS sd
+			ON sd.SensorID = s.ID
+			INNER JOIN data.Visit AS dv
+			ON sd.VisitID = dv.ID
+			LEFT JOIN data.SensorRetrievalAttempt AS sr
+			ON sr.SensorDeploymentID = sd.ID
+			LEFT JOIN data.Site AS ds
+			ON dv.SiteID = ds.ID
+WHERE		sr.ID IS NULL
