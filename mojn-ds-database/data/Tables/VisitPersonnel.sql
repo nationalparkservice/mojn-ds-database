708e360568d903c751c16d715dbc9aa14a94e102
Mark Lehman <mark_lehman@nps.gov>
2019-06-05 08:27:29 -0700

    Removed Personnel foreign key to accomodate using shared table

--- mojn-ds-database/data/Tables/VisitPersonnel.sql
+++ mojn-ds-database/data/Tables/VisitPersonnel.sql
@@ -4,7 +4,6 @@
     [PersonnelRoleID] TINYINT       NOT NULL,
     [DateCreated]     DATETIME2 (0) CONSTRAINT [DF_VisitPersonnel_DateCreated] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_VisitPersonnel] PRIMARY KEY CLUSTERED ([VisitID] ASC, [PersonnelID] ASC, [PersonnelRoleID] ASC),
-    CONSTRAINT [FK_VisitPersonnel_Personnel] FOREIGN KEY ([PersonnelID]) REFERENCES [ref].[Personnel] ([ID]),
     CONSTRAINT [FK_VisitPersonnel_PersonnelRole] FOREIGN KEY ([PersonnelRoleID]) REFERENCES [lookup].[PersonnelRole] ([ID]),
     CONSTRAINT [FK_VisitPersonnel_Visit] FOREIGN KEY ([VisitID]) REFERENCES [data].[Visit] ([ID])
 );
@@ -17,9 +16,6 @@ EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Crossrefere
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Foreign key to data.Visit (information about individual sampling visits)', @level0type = N'SCHEMA', @level0name = N'data', @level1type = N'TABLE', @level1name = N'VisitPersonnel', @level2type = N'COLUMN', @level2name = N'VisitID';
 
-
-GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Foreign key to ref.Personnel (personnel involved in data collection or management)', @level0type = N'SCHEMA', @level0name = N'data', @level1type = N'TABLE', @level1name = N'VisitPersonnel', @level2type = N'COLUMN', @level2name = N'PersonnelID';
 
 
 GO
